72bd2c1d8040949f7f5c614b797d9ffab9d118d0
Richard Ashley <rashley@gmail.com>
2016-01-21 12:30:36 +0000

    Moving to Python3.. just updated autoLogger for now

--- autoLogger.py
+++ autoLogger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse, sys, os, re, json
 import datetime, time, math
@@ -22,7 +22,7 @@ def checkForNewFiles(searchPath):
 	try:
 		files = os.listdir(searchPath)
 	except:
-		print "Could not find the directory %s. Exiting."%searchPath
+		print("Could not find the directory %s. Exiting."%searchPath)
 		sys.exit(-1)
 	for f in files:
 		if f not in fileList:
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 	parser.add_argument('-o', '--outputpath', type=str, help='Path for the JSON output file that is going to be used in the web page.')
 	parser.add_argument('--save', action="store_true", help='Write the input parameters to the config file as default values.')
 	args = parser.parse_args()
-	print args
+	print(args)
 	
 	config = configHelper.configClass("autoLogger")
 	configDefaults  = {
@@ -59,96 +59,109 @@ if __name__ == "__main__":
 		config.save()
 		
 	now = datetime.datetime.now()
-	print "Running autoLogger at:", now
+	print("Running autoLogger at:", now)
 	
 	fileList = []
 	targets = []
-	newFiles = checkForNewFiles(obsdataPath)
 	
-	for f in newFiles: fileList.append(f)
-	# framePattern = re.compile(".*(-|.|_)[0-9]+.(fits.gz|fit.gz|fits|fit|FIT)")
-	framePattern = re.compile("[A-Z,a-z,0-9]*")
-	for filename in newFiles:
-		r = framePattern.search(filename)
-		if (r):
-			targetName = r.group(0)
-			if targetName not in targets: targets.append(targetName)
-	print "Targets:", targets		
-	targetList = []
-	for t in targets:
-		targetObject = {}
-		targetObject['name'] = t
-		targetObject['files'] = []
-		targetObject['frames'] = []
-		for f in fileList:
-			framePattern = re.compile("%s-[0-9]*"%t)
-			r = framePattern.search(f)
+	while True:
+	
+		newFiles = checkForNewFiles(obsdataPath)
+	
+		for f in newFiles: fileList.append(f)
+		framePattern = re.compile(".*(-|.|_)[0-9]+.(fits.gz|fit.gz|fits|fit|FIT)")
+		frameSubPattern = re.compile("[A-Z,a-z,0-9]*")
+		for filename in newFiles:
+			r = framePattern.search(filename)
 			if (r):
-				match = r.group(0)
-				frameNumber = int(match[len(t)+1:])
-				# print frameNumber, match
-				targetObject['files'].append(f)
-				targetObject['frames'].append(frameNumber)
-		print "Frames:", targetObject['frames']
+				rs = frameSubPattern.search(filename)
+				if rs:
+					targetName = rs.group(0)
+					if targetName not in targets: targets.append(targetName)
+		print("Targets:", targets)	
+		targetList = []
+		for t in targets:
+			targetObject = {}
+			targetObject['name'] = t
+			targetObject['files'] = []
+			targetObject['frames'] = []
+			for f in fileList:
+				framePattern = re.compile("%s-[0-9]*"%t)
+				r = framePattern.search(f)
+				if (r):
+					match = r.group(0)
+					frameNumber = int(match[len(t)+1:])
+					targetObject['files'].append(f)
+					targetObject['frames'].append(frameNumber)
 				
-		targetObject['startFrame'] = min(targetObject['frames'])
-		targetObject['endFrame'] = max(targetObject['frames'])
-		targetObject['numFrames'] = len(targetObject['frames'])
+			targetObject['startFrame'] = min(targetObject['frames'])
+			targetObject['endFrame'] = max(targetObject['frames'])
+			targetObject['numFrames'] = len(targetObject['frames'])
 		
-		print targetObject['name'], targetObject['startFrame'], targetObject['endFrame'], targetObject['numFrames']
-		targetObject['files'] = sorted(targetObject['files'])
-		targetObject['frames'] = sorted(targetObject['frames'])
+			print(targetObject['name'], targetObject['startFrame'], targetObject['endFrame'], targetObject['numFrames'])
+			targetObject['files'] = sorted(targetObject['files'])
+			targetObject['frames'] = sorted(targetObject['frames'])
 		
-		targetList.append(targetObject)
+			targetList.append(targetObject)
 		
-	for t in targetList:
-		firstFrame = obsdataPath + '/' + t['files'][0]
-		print "%s: Looking at the FITS headers in %s"%(t['name'], firstFrame)
-		hdulist = fits.open(firstFrame)
-		headers =  hdulist[0].header
-		t['xbin'] = int(headers['XBINNING'])
-		t['ybin'] = int(headers['YBINNING'])
-		t['telescope'] = headers['TELESCOP']
-		t['targetRA'] = headers['OBJRA']
-		t['targetDEC'] = headers['OBJDEC']
-		t['telescopeRA'] = headers['RA']
-		t['telescopeDEC'] = headers['DEC']
-		t['filter'] = headers['FILTER']
-		t['startElevation'] = headers['ELEVATIO']
-		t['startAirmass'] = 1.0/math.cos(math.pi/2.0 - convertDMStoRadians(t['startElevation']))
-		t['exposureTime'] = float(headers['EXPTIME'])
-		t['startMJD'] = float(headers['MJD-OBS'])
-		t['focusPosition'] = headers['FOCUSPOS']
-		if t['filter'] == '': t['filter'] = '--unknown--'	
-		if t['telescope'] == '': t['telescope'] = '--unknown--'	
-		t['xpixels'] = int(headers['NAXIS1'])
-		t['ypixels'] = int(headers['NAXIS2'])
-		hdulist.close()
+		for t in targetList:
+			firstFrame = obsdataPath + '/' + t['files'][0]
+			print("%s: Looking at the FITS headers in %s"%(t['name'], firstFrame))
+			hdulist = fits.open(firstFrame)
+			headers =  hdulist[0].header
+			t['xbin'] = int(headers['XBINNING'])
+			t['ybin'] = int(headers['YBINNING'])
+			t['telescope'] = headers['TELESCOP']
+			t['targetRA'] = headers['OBJRA']
+			t['targetDEC'] = headers['OBJDEC']
+			t['telescopeRA'] = headers['RA']
+			t['telescopeDEC'] = headers['DEC']
+			t['filter'] = headers['FILTER']
+			t['startElevation'] = headers['ELEVATIO']
+			t['startAirmass'] = 1.0/math.cos(math.pi/2.0 - convertDMStoRadians(t['startElevation']))
+			t['exposureTime'] = float(headers['EXPTIME'])
+			t['startMJD'] = float(headers['MJD-OBS'])
+			t['focusPosition'] = headers['FOCUSPOS']
+			if t['filter'] == '': t['filter'] = '--unknown--'	
+			if t['telescope'] == '': t['telescope'] = '--unknown--'	
+			t['xpixels'] = int(headers['NAXIS1'])
+			t['ypixels'] = int(headers['NAXIS2'])
+			t['obsDateTime'] = headers['DATE-OBS']
+			hdulist.close()
 		
-		lastFrame = obsdataPath + '/' + t['files'][-1]
-		print "%s: Looking at the FITS headers in %s"%(t['name'], lastFrame)
-		hdulist = fits.open(lastFrame)
-		headers =  hdulist[0].header
-		t['endElevation'] = headers['ELEVATIO']
-		t['endAirmass'] = 1.0/math.cos(math.pi/2.0 - convertDMStoRadians(t['endElevation']))
-		t['endMJD'] = float(headers['MJD-OBS'])
-		hdulist.close()
+			obsDateTime = datetime.datetime.strptime(t['obsDateTime'], "%Y-%m-%dT%H:%M:%S.%f")
+			t['startObservationUTC'] = obsDateTime.strftime("%H:%M:%S")
 		
-		del t['frames']
-		del t['files']
+			lastFrame = obsdataPath + '/' + t['files'][-1]
+			print("%s: Looking at the FITS headers in %s"%(t['name'], lastFrame))
+			hdulist = fits.open(lastFrame)
+			headers =  hdulist[0].header
+			t['endElevation'] = headers['ELEVATIO']
+			t['endAirmass'] = 1.0/math.cos(math.pi/2.0 - convertDMStoRadians(t['endElevation']))
+			t['endMJD'] = float(headers['MJD-OBS'])
+			t['obsDateTime'] = headers['DATE-OBS']
+			hdulist.close()
+			obsDateTime = datetime.datetime.strptime(t['obsDateTime'], "%Y-%m-%dT%H:%M:%S.%f")
+			t['endObservationUTC'] = obsDateTime.strftime("%H:%M:%S")
 		
-		t['durationMinutes'] = (t['endMJD'] - t['startMJD']) * 24.*60.
 		
-		totalExposureTime = t['exposureTime'] * t['numFrames']
+			del t['frames']
+			del t['files']
 		
-		t['deadTime'] = t['durationMinutes'] * 60. - totalExposureTime
-		t['estimatedReadoutTime'] = t['deadTime'] / t['numFrames']
+			t['durationMinutes'] = (t['endMJD'] - t['startMJD']) * 24.*60.
 		
-	for t in targetList:
-		print t['name']
-		for key in t.keys():
-			print "  ", key, ":", t[key]
+			totalExposureTime = t['exposureTime'] * t['numFrames']
+		
+			t['deadTime'] = t['durationMinutes'] * 60. - totalExposureTime
+			t['estimatedReadoutTime'] = t['deadTime'] / t['numFrames']
+		
+		for t in targetList:
+			print(t['name'])
+			for key in t.keys():
+				print("  ", key, ":", t[key])
 			
-	outputfile = open(jsonPath + "/" + args.date + ".json", 'wt')
-	json.dump(targetList, outputfile)
-	outputfile.close()
+		outputfile = open(jsonPath + "/" + args.date + ".json", 'wt')
+		json.dump(targetList, outputfile)
+		outputfile.close()
+	
+		time.sleep(updateInterval)
---
+++ configHelper.py
@@ -0,0 +1,108 @@
+""" Some functions to aid the setting and retrieving of config files saved in the user's home directory """
+
+import os, json, numpy
+
+class configClass:
+	def __init__(self, name="unknownapp"):
+		self._appName = name
+		self._filename = getUserHome() +"/.config/" + self._appName + "/" + self._appName + ".conf"
+		createConfigFolder(self._appName)
+		self._alreadyExists = False
+		# print "DEBUG: config file is at:", self._filename
+		self.load()
+	
+	def setDefaults(self, defaults):
+		for key in defaults.keys():
+			if key not in self.__dict__.keys():
+				self.setProperty(key, defaults[key])
+		
+	def getProperty(self, name):
+		try:
+			property = getattr(self, name)
+		except AttributeError:
+			property = None
+			print("Could not find value for property", name)
+		return property	
+		
+	def assertProperty(self, name, value):
+		if value!=None:
+			self.setProperty(name, value)
+			print("overriding default", name, value)
+			return value
+		try: 
+			value = getattr(self, name)
+			print("Retrieved value:", name, value)
+		except AttributeError:
+			print("WARNING: %s not specified and no default set."%name)
+			return
+			
+		print(name, value)
+		return value
+
+	def setProperty(self, key, value):
+		setattr(self, key, value)
+		
+					
+	def load(self):
+		filename = self._filename
+		if os.path.exists(filename):
+			self._alreadyExists = True
+		else:
+			print("DEBUG: The configfile, %s, does not exist yet."%filename)
+			return False
+	
+		inputfile = open(filename, "r")
+		jsonObject = json.load(inputfile)
+		for key in jsonObject.keys():
+			keyString = str(key)
+			value = jsonObject[key]
+			print(type(value))
+			# if type(value) is unicode: 
+			#  	value = str(value)
+			if type(value) is list:
+				value = numpy.array(value)
+			print("Loading", key, value)
+			setattr(self, key, value)
+		inputfile.close()
+		return True
+		
+	def save(self):
+		filename = self._filename
+		object = {}
+		for key in self.__dict__.keys():
+			if key[0]=='_': continue         # Don't write properties that start with an underscore
+			data = getattr(self, key)
+			# print key, type(data)
+			if type(data)==numpy.float32:
+				data = float(data)
+			if type(data)==numpy.ndarray:
+				data = numpy.array(data).tolist()
+			if type(data)==list:
+				data = numpy.array(data).tolist()
+			object[key] = data
+			
+		outputfile = open(filename, 'w')
+		json.dump(object, outputfile, indent = 4)
+		outputfile.close()	
+
+def getUserHome():
+	homeDir = os.path.expanduser('~')
+	# print "DEBUG: The user's home directory is %s."%homeDir
+	return homeDir
+
+def createConfigFolder(appName):
+	homeDir = getUserHome()
+	
+	configPath = homeDir + "/.config"
+	if not os.path.exists(configPath):
+		os.mkdir(configPath)
+		print("DEBUG: Creating directory %s"%configPath)
+		
+	fullConfigPath = configPath + "/" + appName
+	if not os.path.exists(fullConfigPath):
+		os.mkdir(fullConfigPath)
+		print("DEBUG: Creating directory %s"%fullConfigPath)
+	
+
+	
+		
